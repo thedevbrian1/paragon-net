f7eb2f01a2850893307ed499a4fea975765338dd
thedevbrian1 <brayomwas95@gmail.com>
2024-06-28 22:01:51 +0300

    Mpesa intgration wip...

--- app/routes/enroll.jsx
+++ app/routes/enroll.jsx
@@ -4,7 +4,7 @@ import { useEffect, useRef } from "react";
 import { useEventSource } from "remix-utils/sse/react";
 import { createClient, requireUser } from "../.server/supabase";
 import Input from "../components/Input";
-import { badRequest, trimValue, } from "../.server/validation";
+import { badRequest, trimValue, validatePhone, } from "../.server/validation";
 import { getSession, sessionStorage, setSuccessMessage } from "../.server/session";
 import { ExclamationIcon } from "../components/Icon";
 import LinkButton from "../components/LinkButton";
@@ -12,10 +12,12 @@ import { createTransaction, getTransactionByCode } from "~/models/transaction";
 import { getStudentByUUID } from "~/models/student";
 import { enrolCourse } from "~/models/enrolment";
 import { getCourseIds } from "~/models/course";
+import FormSpacer from "~/components/FormSpacer";
+import { Label } from "~/components/ui/label";
 
 let steps = ['Payment', 'Enroll'];
 
-export async function loader({ request, response }) {
+export async function loader({ request }) {
     let { user, headers } = await requireUser(request);
     // console.log({ user });
 
@@ -28,9 +30,7 @@ export async function loader({ request, response }) {
     let title = searchParams.get('title');
 
     if (!id || !title) {
-        response.status = 302;
-        response.headers.set('Location', '/courses');
-        throw response;
+        throw redirect('/courses');
     }
 
     let session = await getSession(request);
@@ -46,10 +46,10 @@ export async function loader({ request, response }) {
 
     // console.log({ data });
 
-    return { page, data };
+    return json({ page, data }, { headers });
 }
 
-export async function action({ request, response }) {
+export async function action({ request }) {
     let { supabaseClient, headers } = createClient(request);
 
     let session = await getSession(request);
@@ -78,7 +78,6 @@ export async function action({ request, response }) {
 
                 let formDataPage1 = session.get('form-data-page-1');
                 let clientPhone = formDataPage1?.mpesa;
-                response.headers.set("Set-Cookie", await sessionStorage.commitSession(session));
 
                 // Record entries only if the returned number is equal to the number provided
                 if (mpesaCode && transactionPhone === clientPhone) {
@@ -100,14 +99,17 @@ export async function action({ request, response }) {
                         ...Object.fromEntries(studentHeaders.entries()),
                         "Set-Cookie": await sessionStorage.commitSession(session)
                     };
-                    response.status = 302;
-                    response.headers.set('Location', `${url.href}`);
-                    response.headers.set("Set-Cookie", await sessionStorage.commitSession(session));
 
-                    return response;
+                    return json({ ok: true }, {
+                        headers: allHeaders
+                    });
                 }
 
-                return { ok: true };
+                return json({ ok: true }, {
+                    headers: {
+                        "Set-Cookie": await sessionStorage.commitSession(session)
+                    }
+                });
             }
 
             // Initiate stk push from the client's phone
@@ -137,7 +139,7 @@ export async function action({ request, response }) {
 
 
                 // Generate access token
-                let accessTokenRes = await fetch('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', {
+                let accessTokenRes = await fetch('https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', {
                     method: 'GET',
                     headers: {
                         Authorization: `Basic ${btoa(`${process.env.SAFARICOM_CONSUMER_KEY}:${process.env.SAFARICOM_SECRET_KEY}`)}`
@@ -146,9 +148,11 @@ export async function action({ request, response }) {
                 let token = await accessTokenRes.json();
                 let accessToken = token.access_token;
 
+                console.log({ accessToken });
+
                 // Initiate stk push
                 let modifiedPhone = null;
-                response.headers.set("Set-Cookie", await sessionStorage.commitSession(session));
+
                 if (trimmedPhone.length === 10) {
                     modifiedPhone = trimmedPhone.replace(0, "254");
                 } else if (trimmedPhone.length === 13) {
@@ -163,32 +167,35 @@ export async function action({ request, response }) {
                 let timeStamp = String(dateString + now).replace(/\D+/g, '');
 
 
-                let passwordString = '174379' + 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919' + timeStamp;
+                let passwordString = '4138431' + 'cdf212732d649e6199b8fa93a0b7619c0a1a5a202b850d6e90b7263c4db2ce32' + timeStamp;
 
                 let encodedPassword = Buffer.from(passwordString).toString('base64');
 
-                let res = await fetch('https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest', {
+                let res = await fetch('https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest', {
                     method: "post",
                     headers: {
                         "Content-Type": "application/json",
                         Authorization: `Bearer ${accessToken}`
                     },
                     body: JSON.stringify({
-                        "BusinessShortCode": 174379,
+                        "BusinessShortCode": 4138431,
                         "Password": encodedPassword,
                         "Timestamp": timeStamp,
                         "TransactionType": "CustomerPayBillOnline",
                         "Amount": 1,
                         "PartyA": modifiedPhone,
-                        "PartyB": 174379,
+                        "PartyB": 4138431,
                         "PhoneNumber": modifiedPhone,
-                        "CallBackURL": "https://thick-jobs-fail.loca.lt/saf",
-                        "AccountReference": "Paragon",
+                        "CallBackURL": "https://bd56-41-80-116-90.ngrok-free.app/saf",
+                        "AccountReference": "ParagonShell LTD",
                         "TransactionDesc": "Course Payment"
                     })
                 });
 
                 let resBody = await res.json();
+                console.log({ resBody });
+
+                return null;
                 if (resBody.errorCode) {
                     throw new Error(resBody.errorMessage);
                 }
@@ -203,7 +210,11 @@ export async function action({ request, response }) {
                 let data = { mpesa: modifiedPhone };
                 session.set(`form-data-page-${page}`, data);
 
-                return { ok: true };
+                return json({ ok: true }, {
+                    headers: {
+                        "Set-Cookie": await sessionStorage.commitSession(session)
+                    }
+                });
             }
         }
         case 2: {
@@ -221,9 +232,7 @@ export async function action({ request, response }) {
             // Check if course is valid
             let courseId = searchParams.get('id');
             let courses = await getCourseIds();
-            response.status = 302;
-            response.headers.set('Location', '/schedule');
-            response.headers.set("Set-Cookie", await sessionStorage.commitSession(session));
+
             let courseIds = courses.result.map(course => course._id);
 
             if (!courseIds.includes(courseId)) {
@@ -260,25 +269,18 @@ export async function action({ request, response }) {
                 "Set-Cookie": await sessionStorage.commitSession(session)
 
             }
-            return response;
-        }
-        case 4: {
-            // Clear the input fields from the session
-            session.unset('form-data-page-1');
-            session.unset('form-data-page-2');
-            response.status = 302;
-            response.headers.set('Location', '/schedule');
-            response.headers.set("Set-Cookie", await sessionStorage.commitSession(session));
-            session.unset('form-data-page-3');
-            return response;
+            return redirect('/schedule', {
+                headers: allHeaders
+            });
         }
 
     }
-    response.status = 302;
-    response.headers.set('Location', `?page=${nextPage}`);
-    response.headers.set("Set-Cookie", await sessionStorage.commitSession(session));
 
-    return response;
+    return redirect(`?page=${nextPage}`, {
+        headers: {
+            "Set-Cookie": await sessionStorage.commitSession(session)
+        }
+    });
 }
 
 export default function Enroll() {
@@ -322,7 +324,7 @@ export default function Enroll() {
 
     return (
         <main className="max-w-3xl lg:max-w-xl mx-auto px-4 sm:px-6 lg:px-8 text-brand-black">
-            <div className="border border-gray-100 mt-8 lg:mt-24 p-8 rounded">
+            <div className="border border-gray-100 shadow mt-8 lg:mt-24 p-8 rounded">
                 <h1 className="font-bold text-xl">Enroll for course ({title})</h1>
                 <div className="flex gap-4 mt-2">
                     {steps.map((step, index) => (
@@ -332,29 +334,46 @@ export default function Enroll() {
                         >{index + 1}) {step}</span>
                     ))}
                 </div>
+
                 <Form method="post" className="mt-4">
                     {Number(page) === 1
                         ? (
-                            <fieldset>
-                                <h2 className="font-semibold">Payment</h2>
+                            <>
                                 <div>
-                                    <label htmlFor="mpesa">MPESA number</label>
-                                    <Input
-                                        type="text"
-                                        name="mpesa"
-                                        id="mpesa"
-                                        placeholder="0712 345 678"
-                                        defaultValue={data?.mpesa}
-                                        fieldError={actionData?.fieldErrors?.mpesa}
-                                        ref={mpesaRef}
-                                    />
+                                    <div className="flex flex-col md:flex-row items-center md:gap-4">
+                                        <h2 className="font-semibold text-lg">Pay using MPESA</h2>
+                                        <img src="/lipa-na-mpesa.png" alt="" className="w-44" />
+                                    </div>
+                                    <p className="bg-orange-50 text-orange-500 px-6 py-3 rounded-md">Please check your phone for STK menu</p>
+                                    <h3 className="font-semibold mt-4">Steps for making payment</h3>
+                                    <ol className="list-decimal mt-4 pl-4 space-y-2">
+                                        <li>Enter your MPESA phone number in the input below and click 'Submit'.</li>
+                                        <li>Check your phone. You will receive a prompt. Enter your MPESA PIN and click 'Ok'.</li>
+                                        <li>You will receive a confirmation SMS from MPESA.</li>
+                                    </ol>
                                 </div>
-                            </fieldset>
+                                <fieldset className="mt-4">
+                                    {/* <h3 className="font-semibold">Payment</h3> */}
+                                    <FormSpacer>
+                                        <Label htmlFor="mpesa">MPESA number</Label>
+                                        <Input
+                                            type="text"
+                                            name="mpesa"
+                                            id="mpesa"
+                                            placeholder="0712 345 678"
+                                            defaultValue={data?.mpesa}
+                                            fieldError={actionData?.fieldErrors?.mpesa}
+                                            ref={mpesaRef}
+                                        />
+                                    </FormSpacer>
+                                </fieldset>
+                            </>
+
                         )
                         : Number(page) === 2
                             ? (
                                 <fieldset>
-                                    <h2 className="font-semibold">Enroll course</h2>
+                                    <h3 className="font-semibold">Enroll course</h3>
                                     <p>You will be enrolled in the course {title}</p>
                                     <button
                                         type="submit"
@@ -390,7 +409,7 @@ export default function Enroll() {
                                 name="_action"
                                 value="next"
                                 className="bg-brand-orange hover:bg-orange-400 focus:border-none focus:outline-none focus:ring-2 focus:ring-brand-black transition duration-300 ease-in-out text-black rounded capitalize px-4 py-2">
-                                {isSubmitting ? 'Processing...' : 'Next'}
+                                {isSubmitting ? 'Processing...' : 'Submit'}
                             </button>
                         )}
                         {/* {(Number(page) === 4) && (
